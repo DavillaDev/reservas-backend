b82064ef152b355a36b211fb629fc5d5df8dc67c
Thúlio D'avilla <thulioalmeida57@gmail.com>
2025-12-17 11:03:58 -0300

    ajuste

--- src/nightclubs/nightclubs.module.ts
+++ src/nightclubs/nightclubs.module.ts
@@ -1,22 +1,17 @@
-// api/src/nightclubs/nightclubs.module.ts (CORRIGIDO PARA O MASTER AUTH)
-
 import { Module } from '@nestjs/common';
 import { NightclubsService } from './nightclubs.service';
 import { NightclubsController } from './nightclubs.controller';
 import { PrismaService } from '../prisma.service';
 import { MasterAuthGuard } from '../super/guards/master-auth.guard';
-import { ConfigModule } from '@nestjs/config'; // 🔑 1. IMPORTAÇÃO DO GUARD MASTER
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
-    // 🔑 ADICIONE ESTA LINHA: Torna o ConfigService disponível
-    ConfigModule.forRoot({ isGlobal: true }), // Se já for global no AppModule, basta ConfigModule
+    // 🛡️ O ConfigModule deve estar aqui para o ConfigService funcionar no Service
+    ConfigModule,
   ],
   controllers: [NightclubsController],
-  providers: [
-    NightclubsService,
-    PrismaService,
-    MasterAuthGuard, // 🔑 2. ADICIONA O GUARD AOS PROVIDERS
-  ],
+  providers: [NightclubsService, PrismaService, MasterAuthGuard],
+  exports: [NightclubsService], // Importante para que outros módulos acessem este service
 })
 export class NightclubsModule {}
