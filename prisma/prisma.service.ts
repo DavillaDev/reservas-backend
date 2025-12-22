8e4194d881c906c085987cdae5d069bf30bdec11
Thúlio D'avilla <thulioalmeida57@gmail.com>
2025-12-22 18:33:42 -0300

    ajuste

--- prisma/prisma.service.ts
+++ prisma/prisma.service.ts
@@ -6,8 +6,28 @@ export class PrismaService
   extends PrismaClient
   implements OnModuleInit, OnModuleDestroy
 {
+  constructor() {
+    // 👇 AQUI NÓS FORÇAMOS A CONFIGURAÇÃO
+    super({
+      datasources: {
+        db: {
+          url: process.env.DATABASE_URL,
+        },
+      },
+      // 👇 Aumentamos a paciência do banco para logs e conexões
+      log: ['error', 'warn'],
+    });
+  }
+
   async onModuleInit() {
-    await this.$connect();
+    // 👇 Tentativa de conexão com retry manual (Resiliência)
+    try {
+      await this.$connect();
+    } catch (error) {
+      console.warn(
+        '⚠️ Falha inicial no DB. O Nest vai tentar de novo na primeira requisição.',
+      );
+    }
   }
 
   async onModuleDestroy() {
