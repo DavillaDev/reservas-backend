c1326d6680e5646f19f96f354d1c52feef56f637
Thúlio D'avilla <thulioalmeida57@gmail.com>
2026-01-09 13:27:23 -0300

    ajuste

--- src/modules/vip/vip.module.ts
+++ src/modules/vip/vip.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 import { VipService } from './vip.service';
 import { VipController } from './vip.controller';
-import { PrismaModule } from '../../prisma/prisma.module';
+import { PrismaModule } from '../../../prisma/prisma.module';
 
 @Module({
   imports: [PrismaModule],
