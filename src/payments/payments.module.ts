2a3e96a37aaba8bef08a7faf82984d20793f17a1
Thúlio D'avilla <thulioalmeida57@gmail.com>
2026-02-21 11:06:35 -0300

    fix: injeta mail service e prisma no payments module de verdade

--- src/payments/payments.module.ts
+++ src/payments/payments.module.ts
@@ -2,9 +2,20 @@ import { Module } from '@nestjs/common';
 import { PaymentsService } from './payments.service';
 import { PaymentsController } from './payments.controller';
 
+// 👇 Importa o Service direto (já que não tem módulo)
+import { MailService } from '../mail/mail.service';
+// 👇 Importa o Módulo de Notificações (esse eu vi que existe!)
+import { NotificationsModule } from '../notifications/notifications.modules';
+
 @Module({
+  imports: [
+    NotificationsModule, // 👈 Importa só o módulo que existe
+  ],
   controllers: [PaymentsController],
-  providers: [PaymentsService],
-  exports: [PaymentsService], // Exportamos caso outro módulo precise acessar os pagamentos no futuro
+  providers: [
+    PaymentsService,
+    MailService, // 👈 Injeta o MailService solto direto aqui!
+  ],
+  exports: [PaymentsService],
 })
 export class PaymentsModule {}
